cf4ecf092425b31d681c2aac943dcde560b1a08a
VIKASH KUMAR <vikash.11803824@lpu.in>
2020-04-04 14:47:26 +0530

    adding problem statement

--- OS_p1.c
+++ OS_p1.c
@@ -1,3 +1,10 @@
+/* Design a scheduling program that is capable of scheduling many processes that comes in at some time interval 
+  and are allocated the CPU not more than 10 time units.CPU must schedule processes having short execution time first. 
+  CPU is idle for 3 time units and does not entertain any process prior this time.
+  Scheduler must maintain a queue that keeps the order of execution of all the processes. Compute average waiting and turnaround time.
+ */
+
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<conio.h> 
